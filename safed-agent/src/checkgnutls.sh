d3a17345d2d5487c97e03b484f7bac9bf32e6cd4
root <root@neteye4-mamh.wp.lan>
2019-02-27 12:23:31 +0100

    includ wolfssl headers

--- safed-agent/src/checkgnutls.sh
+++ safed-agent/src/checkgnutls.sh
@@ -2,24 +2,17 @@ if [ $# -eq 0 ]
 then
         LIBWOLFTLS="libwolfssl.so.19"
         # >= libwolfssl.so.19.0.0 
-        if [ -d "/usr/include/wolfssl" ];then
-                # .pc are not available for all libs!!
-                #pkg-config --exists --print-errors "gnutls >= 3.6.4"
-                #if [ $? -eq 0 ];then exit 0;else exit 1;fi
-                for d in /usr/lib64/ /usr/lib/ /lib /lib64
-                do  
-                    find $d |grep -E "$LIBWOLFTLS" > /dev/null
-                    if [ $? -eq 0 ];then echo "wolfssl"; exit 0;fi
-                done
-                echo ""    
-        else
-                echo ""
-        fi
+        for d in /usr/lib64/ /usr/lib/ /lib /lib64
+        do  
+            find $d |grep -E "$LIBWOLFTLS" > /dev/null
+            if [ $? -eq 0 ];then echo "wolfssl"; exit 0;fi
+        done
+        echo ""    
 elif [ "$1" = "wolfssl" ] 
 then
         if [ "$2" = "-d" ] 
 	then
-	       echo "-D TLSPROTOCOL -DWC_RSA_BLINDING -DWOLFSSL_TLS13"
+	       echo "-I../external/wolfssl/wolfssl/ -DTLSPROTOCOL -DWC_RSA_BLINDING -DWOLFSSL_TLS13"
 	elif [ "$2" = "-c" ]
 	then
 		echo "SafedTLS.o"
