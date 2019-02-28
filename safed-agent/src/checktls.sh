40e491070640fc979ad56f72643a834c8e69f432
root <root@neteye4-mamh.wp.lan>
2019-02-28 16:14:47 +0100

    rename check gnitls to wolftls

---
+++ safed-agent/src/checktls.sh
@@ -0,0 +1,35 @@
+if [ $# -eq 0 ]
+then
+        LIBWOLFTLS="libwolfssl.so.19"
+        # >= libwolfssl.so.19.0.0 
+        # uname | grep "Linux" > /dev/null
+        if [ -f /sbin/ldconfig ];then
+            /sbin/ldconfig -p|grep -E "$LIBWOLFTLS" > /dev/null
+            if [ $? -eq 0 ];then echo "wolfssl"; exit 0;fi
+        else 
+            for d in /usr/lib64/ /usr/lib/ /lib /lib64 /usr/local/lib /usr/local/lib64
+            do 
+                if [ -d $d ]; then 
+                    find $d |grep -E "$LIBWOLFTLS" > /dev/null
+                    if [ $? -eq 0 ];then echo "wolfssl"; exit 0;fi
+                fi
+            done
+        fi
+        echo ""    
+elif [ "$1" = "wolfssl" ] 
+then
+        if [ "$2" = "-d" ] 
+	then
+	       echo "-I../external/wolfssl/wolfssl/ -DTLSPROTOCOL -DWC_RSA_BLINDING -DWOLFSSL_TLS13"
+	elif [ "$2" = "-c" ]
+	then
+		echo "SafedTLS.o"
+	elif [ "$2" = "-l" ]
+ 	then
+		echo "-lwolfssl"
+	else
+		echo ""
+	fi
+else
+	echo ""
+fi
